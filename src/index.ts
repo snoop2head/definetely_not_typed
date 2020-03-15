6afd3bc094772e7616663cfde5e03c47ddbebed0
noopy <young_ahn@yonsei.ac.kr>
2020-03-15 11:55:39 +0900

    using class to designate each argument's types & pass class onto index.js

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // designating each argument's types with interface
+/*
 interface Human {
   name: string;
   age: number;
@@ -12,11 +13,27 @@ const person = {
   language: "python"
 };
 
+*/
+
+// pass properties onto index.js and designate each argument's types with class
+class Human {
+  public name: string;
+  public age: number;
+  public language: string;
+  constructor(name: string, age: number, language: string) {
+    this.name = name;
+    this.age = age;
+    this.language = language;
+  }
+}
+
+const myam = new Human("myam", 17, "IFRS");
+
 // function
 const sayNoop = (person: Human): string => {
   return `hello ${person.name}, you are ${person.age} old and you love ${person.language}`;
 };
 
-console.log(sayNoop(person));
+console.log(sayNoop(myam));
 
 export {};
